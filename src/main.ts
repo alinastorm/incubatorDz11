2b9356037865c09281f7412217695b7f081fd1ab
AlexGr <753464@gmail.com>
2022-11-28 22:12:19 +0300

    1.0.1

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import mongoDbClient from './_common/services/mongoDb/mongoDbClient'
+import emailService from "./_common/services/email/email-service";
+import periodicTasks from "./_common/services/taskManager/periodicTasks-service";
+import httpService from './_common/services/http/http-service';
+
+
+
+await mongoDbClient.connect()
+await mongoDbClient.ping()
+await emailService.connect()
+periodicTasks.run()
+httpService.setMiddlewares()
+httpService.setRoutes()
+httpService.runHttpsServer()
+// httpService.runHttpServer()
